a23dabc754c874eb96de99fce8a7fe1cdeb8807f
Bernard Heuse <bheuse@amdocs.com>
2022-03-01 18:51:04 +0100

    Added Custom Context Added Include File Def. Support

---
+++ NEF/API_Data_Model_Sample/API_Data_Model_Sample_templates/ServicesCatalogManagerImpl.java
@@ -0,0 +1,211 @@
+/* THIS IS AUTO GENERATED CODE. DO NOT CHANGE. CHANGE ARCHITECT SOURCE INSTEAD */
+\n
+package com.openet.servicescatalogmanager.impl;
+\n
+import com.openet.modules.framework.voltdb.client.VoltClient;
+import com.openet.modules.framework.voltdb.client.VoltProcedure;
+import com.openet.modules.framework.voltdb.client.impl.VoltClientImpl;
+import com.openet.modules.nef.servicescatalogservice.service.api.beans.DatastoreApiBeans.*;
+import com.openet.modules.nef.servicescatalogservice.service.api.model.*;
+import com.openet.modules.opc.common.enums.HttpStatus;
+import com.openet.sba.core.flowcontext.api.FlowContext;
+import com.openet.sba.core.provider.annotations.ProviderActivate;
+import com.openet.sba.core.provider.annotations.ProviderEnabledComponent;
+import com.openet.sba.core.provider.annotations.ProviderInjectedConfig;
+import com.openet.sba.core.provider.enums.ProviderEnabledComponentType;
+import com.openet.servicescatalogmanager.api.ServicesCatalogManager;
+import com.openet.stages.common.exception.NefStageException;
+import io.reactivex.Completable;
+import io.reactivex.Scheduler;
+import io.reactivex.Single;
+import io.vertx.core.json.Json;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.voltdb.VoltTable;
+import org.voltdb.client.ClientResponse;
+\n
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+import java.util.concurrent.CompletableFuture;
+\n
+import static com.openet.modules.opc.common.enums.HttpStatus.INTERNAL_SERVER_ERROR;
+import static com.openet.modules.opc.common.enums.HttpStatus.NOT_FOUND;
+\n
+@ProviderEnabledComponent(configurationPid = "ServicesCatalogManager", componentType = ProviderEnabledComponentType.COMPONENT)
+public class ServicesCatalogManagerImpl implements ServicesCatalogManager {
+\n
+    private final Logger logger = LoggerFactory.getLogger(this.getClass());
+\n
+    private VoltClient voltClient;
+\n
+    @ProviderInjectedConfig
+    private ServicesCatalogManagerConfig config;
+\n
+    @ProviderActivate
+    public void activate() throws Exception {
+        voltClient = new VoltClientImpl(config.getVoltConfig());
+    }
+\n
+% for ENTITY, ENTITY_DATA in ENTITIES.items():
+<% className = ENTITY.replace('_', '') %>
+% if 'PATH' in ENTITY_DATA:
+% if ENTITY_DATA['PATH_OPERATION'] != 'read-only':
+    @Override
+    public Single<${className}> create${className}(${className} entity, FlowContext ctx) {
+        entity.setId(UUID.randomUUID().toString());
+\n
+        String procedureName = "NEF_create${ENTITY}";
+        return executeProcedureRx(ctx,
+                procedureName,
+                entity.getId(),
+                Json.encode(entity))
+                .doOnSuccess(response -> logResponse(procedureName, response, ctx))
+                .doOnSuccess(response -> checkResponseStatus(procedureName, response, ctx))
+                .map(response -> entity);
+    }
+\n
+    @Override
+    public Single<${className}> update${className}(${className} entity, FlowContext ctx) {
+        String procedureName = "NEF_update${ENTITY}";
+        return executeProcedureRx(ctx,
+                procedureName,
+                Json.encode(entity),
+                entity.getId())
+                .doOnSuccess(response -> logResponse(procedureName, response, ctx))
+                .doOnSuccess(response -> checkResponseStatus(procedureName, response, ctx))
+                .doOnSuccess(response -> checkEntityFoundByModifiedCount(procedureName, response, ctx))
+                .map(response -> entity);
+    }
+\n
+    @Override
+    public Completable delete${className}(String id, FlowContext ctx) {
+        String procedureName = "NEF_delete${ENTITY}";
+        return executeProcedureRx(ctx, procedureName, id)
+                .doOnSuccess(response -> logResponse(procedureName, response, ctx))
+                .doOnSuccess(response -> checkResponseStatus(procedureName, response, ctx))
+                .doOnSuccess(response -> checkEntityFoundByModifiedCount(procedureName, response, ctx))
+                .ignoreElement();
+    }
+\n
+% endif
+    @Override
+    public Single<${className}> get${className}(String id, FlowContext ctx) {
+        String procedureName = "NEF_get${ENTITY}";
+        return executeProcedureRx(ctx, procedureName, id)
+                .doOnSuccess(response -> logResponse(procedureName, response, ctx))
+                .doOnSuccess(response -> checkResponseStatus(procedureName, response, ctx))
+                .doOnSuccess(response -> checkEntityFoundByRowCount(procedureName, response, ctx))
+                .map(response -> {
+                    VoltTable table = response.getResults()[0];
+                    table.resetRowPosition();
+                    table.advanceRow();
+\n
+                    ${className} entity = Json.decodeValue(table.getString(1), ${className}.class);
+                    entity.setId(table.getString(0));
+                    return entity;
+                });
+    }
+\n
+    @Override
+    public Single<List<${className}>> getAll${className}(Get${className}sQueryParams queryParams, FlowContext ctx) {
+        String procedureName = "NEF_getAll${ENTITY}";
+        return executeProcedureRx(ctx, procedureName,
+                ${generateFilterParams(ENTITY_DATA)}
+                queryParams.getLimit() != null ? queryParams.getLimit() : Integer.MAX_VALUE,
+                queryParams.getOffset() != null ? queryParams.getOffset() : 0)
+                .doOnSuccess(response -> logResponse(procedureName, response, ctx))
+                .doOnSuccess(response -> checkResponseStatus(procedureName, response, ctx))
+                .map(response -> {
+                    List<${className}> entities = new ArrayList<>();
+\n
+                    VoltTable table = response.getResults()[0];
+                    table.resetRowPosition();
+                    while (table.advanceRow()) {
+                        ${className} entity = Json.decodeValue(table.getString(1), ${className}.class);
+                        entity.setId(table.getString(0));
+                        entities.add(entity);
+                    }
+\n
+                    return entities;
+                });
+    }
+\n
+% endif
+% endfor
+\n
+    private Single<ClientResponse> executeProcedureRx(FlowContext ctx, String name, Object... request) {
+        logger.trace(ctx.getMarker(), "executeProcedureRx(name:{}, request:{})", name, request);
+        return toRx(executeProcedure(name, request), ctx.getScheduler())
+                .onErrorResumeNext(throwable -> Single.error(procedureError(INTERNAL_SERVER_ERROR, name, "error",
+                        throwable, ctx)));
+    }
+\n
+    private CompletableFuture<ClientResponse> executeProcedure(String name, Object... request) {
+        return voltClient.executeProcedure(new VoltProcedure(name, request));
+    }
+\n
+    public static <T> Single<T> toRx(CompletableFuture<T> future, Scheduler scheduler) {
+        Single<T> single = Single.create(emitter -> future
+                .thenAccept(emitter::onSuccess)
+                .exceptionally(error -> {
+                    emitter.onError(error);
+                    return null;
+                }));
+        return single.observeOn(scheduler);
+    }
+\n
+    private void logResponse(String procedureName, ClientResponse clientResponse, FlowContext ctx) {
+        logger.trace(ctx.getMarker(), "logResponse(procedureName:{}, status:{}, results:{})", procedureName,
+                clientResponse.getStatus(), clientResponse.getResults());
+    }
+\n
+    private void checkResponseStatus(String procedureName, ClientResponse clientResponse, FlowContext ctx) {
+        if (clientResponse.getStatus() != ClientResponse.SUCCESS) {
+            throw procedureError(INTERNAL_SERVER_ERROR, procedureName, "status",
+                    clientResponse.getAppStatusString(), ctx);
+        }
+    }
+\n
+    private void checkEntityFoundByRowCount(String procedureName, ClientResponse clientResponse, FlowContext ctx) {
+        if (clientResponse.getResults()[0].getRowCount() != 1) {
+            throw notFoundError(procedureName, ctx);
+        }
+    }
+\n
+    private void checkEntityFoundByModifiedCount(String procedureName, ClientResponse clientResponse, FlowContext ctx) {
+        VoltTable table = clientResponse.getResults()[0];
+        table.resetRowPosition();
+        if (!table.advanceRow() || table.getLong(0) != 1) {
+            throw notFoundError(procedureName, ctx);
+        }
+    }
+\n
+    private NefStageException notFoundError(String procedureName, FlowContext ctx) {
+        return procedureError(NOT_FOUND, procedureName, "error", "No entity found", ctx);
+    }
+
+    private NefStageException procedureError(HttpStatus httpStatus, String procedureName, String cause, Object details,
+                                             FlowContext ctx) {
+        String message = String.format("Procedure '%s' execution failed with %s: %s", procedureName, cause, details);
+        logger.error(ctx.getMarker(), message);
+        return new NefStageException(httpStatus, null, cause, message);
+    }
+}
+
+<%def name="generateFilterParams(entityData)">
+<%
+    def isFilterParam(propData):
+        return 'Schema' in propData and propData['Schema']['filter']
+
+    def getterName(prop):
+        prop = prop.replace('_', '')
+        return 'get' + prop[0].upper() + prop[1].lower() + prop[2:]
+%>
+        % for prop, propData in entityData['properties'].items():
+            % if isFilterParam(propData):
+                queryParams.${getterName(prop)}(),
+                queryParams.${getterName(prop)}(),
+            % endif
+        % endfor
+</%def>
---
+++ NEF/API_Data_Model_Sample/API_Data_Model_Sample_templates/ServicesCatalogService.feature
@@ -0,0 +1,221 @@
+# THIS IS AUTO GENERATED CODE. DO NOT CHANGE. CHANGE ARCHITECT SOURCE INSTEAD
+\n
+@ServicesCatalogService
+Feature: Services Catalog Service operations
+\n
+  Background:
+    * url apiBaseUrl
+<% deleteFromTablesQuery = '; '.join(['DELETE FROM NEF_' + ENTITY for ENTITY, ENTITY_DATA in ENTITIES.items() if 'PATH' in ENTITY_DATA]) %>
+    * configure afterScenario =
+      """
+      function() {
+        karate.call('classpath:util/execute-query.feature', { query: '${deleteFromTablesQuery};' })
+      }
+      """
+\n
+% for ENTITY, ENTITY_DATA in ENTITIES.items():
+
+% if 'PATH' in ENTITY_DATA and ENTITY_DATA['PATH_OPERATION'] != 'read-only':
+
+<% path = ENTITY_DATA['PATH'] + 's' %>
+
+  Scenario: Get ${ENTITY} by id
+    Given path '/datastore/${path}'
+    And def entity =
+      """
+      ${generateEntityJson(ENTITY_DATA)}
+      """
+    And request entity
+    When method post
+    And print response
+    And status 202
+    And eval entity.id = response.id
+    Then match response == entity
+\n
+    Given path '/datastore/${path}/' + entity.id
+    When method get
+    And print response
+    And status 200
+    Then match response == entity
+\n
+  Scenario: Get ${ENTITY} by id not found
+    Given path '/datastore/${path}/dummy'
+    When method get
+    And print response
+    And status 404
+\n
+  Scenario: Get All ${ENTITY}
+    When def entity =
+      """
+      ${generateEntityJson(ENTITY_DATA)}
+      """
+\n
+    Given path '/datastore/${path}'
+    And request entity
+    When method post
+    And print response
+    And status 202
+\n
+    Given path '/datastore/${path}'
+    And request entity
+    When method post
+    And print response
+    And status 202
+\n
+    Given path '/datastore/${path}'
+    When method get
+    Then status 200
+    And print response
+    And assert response.length == 2
+\n
+    Given path '/datastore/${path}'
+    And param limit = 1
+    And param offset = 0
+    When method get
+    Then status 200
+    And print response
+    And assert response.length == 1
+\n
+    Given path '/datastore/${path}'
+    And param limit = 1
+    And param offset = 1
+    When method get
+    Then status 200
+    And print response
+    And assert response.length == 1
+\n
+    Given path '/datastore/${path}'
+    And param limit = 1
+    And param offset = 2
+    When method get
+    Then status 200
+    And print response
+    And assert response.length == 0
+\n
+  Scenario: Update ${ENTITY} by id
+    Given path '/datastore/${path}'
+    And request
+      """
+      ${generateEntityJson(ENTITY_DATA)}
+      """
+    When method post
+    And print response
+    And status 202
+\n
+    And def updatedEntity =
+      """
+      ${generateEntityJson(ENTITY_DATA)}
+      """
+    And eval updatedEntity.id = response.id
+\n
+    Given path '/datastore/${path}/' + updatedEntity.id
+    And request updatedEntity
+    When method put
+    And print response
+    And status 202
+    Then match response == updatedEntity
+\n
+  Scenario: Update ${ENTITY} by id not found
+    Given path '/datastore/${path}/dummy'
+    And request
+      """
+      ${generateEntityJson(ENTITY_DATA)}
+      """
+    When method put
+    And print response
+    And status 404
+\n
+  Scenario: Delete ${ENTITY} by id
+    Given path '/datastore/${path}'
+    And request
+      """
+      ${generateEntityJson(ENTITY_DATA)}
+      """
+    When method post
+    And print response
+    And status 202
+    And def id = response.id
+\n
+    Given path '/datastore/${path}/' + id
+    When method delete
+    And print response
+    And status 204
+\n
+    Given path '/datastore/${path}/' + id
+    When method get
+    And print response
+    And status 404
+\n
+  Scenario: Delete ${ENTITY} by id not found
+    Given path '/datastore/${path}/dummy'
+    When method delete
+    And print response
+    And status 404
+\n
+% endif
+% endfor
+
+<%def name="generateEntityJson(entityData)">
+<%
+    import random, string
+
+    def randomString(length=10):
+        letters = string.ascii_lowercase
+        return ''.join(random.choice(letters) for i in range(length))
+
+    def randomBoolean():
+        return random.choice([True, False])
+
+    def randomInt(begin=0, end=100):
+        return random.randint(begin, end)
+
+    def now():
+        import datetime
+        return datetime.datetime.now().replace(microsecond=0).isoformat() + 'Z'
+
+    def randomValue(type, format, subType, subFormat):
+        if type == 'string':
+            return now() if format == 'date-time' else randomString()
+        elif type == 'boolean':
+            return randomBoolean()
+        elif type == 'integer':
+            return randomInt()
+        elif type == 'array':
+            return [randomValue(subType, subFormat, None, None)]
+        else:
+            return None
+
+    def doGenerateRefEntity(container):
+        subEntity = container['$ref'].split('/')[-1]
+        return doGenerateEntity(ENTITIES[subEntity])
+        
+    def doGenerateEntity(entityData):
+        entity = {}
+
+        for prop, propData in entityData['properties'].items():
+            if '$ref' in propData:
+                entity[prop] = doGenerateRefEntity(propData)
+            elif propData['type'] == 'array' and '$ref' in propData['items']:
+                entity[prop] = [doGenerateRefEntity(propData['items'])]
+            else:
+                type = propData['type']
+                format = propData['format'] if 'format' in propData else None
+                subType = None
+                subFormat = None
+                if type == 'array':
+                    items = propData['items']
+                    subType   = items['type']   if 'type'   in items else 'string'
+                    subFormat = items['format'] if 'format' in items else None
+
+                entity[prop] = randomValue(type, format, subType, subFormat)
+
+        return entity
+
+    def doGenerateEntityJson(entityData):
+        import json
+        return json.dumps(doGenerateEntity(entityData), indent=2)
+%>
+    % for line in doGenerateEntityJson(entityData).splitlines():
+      ${line}
+    % endfor
+</%def>
